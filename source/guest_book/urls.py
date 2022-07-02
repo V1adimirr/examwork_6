b6e3fab68678f0063934329bed58922472339c11
Vladimir <vladimirpedenko936@gmail.com>
2022-07-02 18:22:43 +0600

    "добавлено редактирование записи"

--- source/guest_book/urls.py
+++ source/guest_book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from guest_book.views import index_view, delete_view, create_view
+from guest_book.views import index_view, delete_view, create_view, update_view
 
 urlpatterns = [
     path('', index_view, name="index"),
     path('entry/add/', create_view, name="create"),
+    path('enty/<int:pk>/update/', update_view, name="update"),
     path('entry/<int:pk>/delete/', delete_view, name="delete")
 ]
--- source/guest_book/views.py
+++ source/guest_book/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
 from guest_book.models import Entry
 
 
@@ -18,6 +17,19 @@ def create_view(request):
         content = request.POST.get("content")
         new_entry = Entry.objects.create(author=author, author_mail=author_mail, content=content)
         new_entry.save()
+        return redirect("index")
+
+
+def update_view(request, pk):
+    entries = get_object_or_404(Entry, pk=pk)
+    if request.method == "GET":
+        return render(request, "update.html", {"entry": entries})
+    else:
+        entries.author = request.POST.get("author")
+        entries.author_mail = request.POST.get("author_mail")
+        entries.content = request.POST.get("content")
+        new_entries = Entry.objects.create(author=entries.author, author_mail=entries.author_mail, content=entries.content)
+        new_entries.save()
         return redirect("index")
 
 
